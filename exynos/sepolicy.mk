0b71cbb3e87ba9fca5ed0a2baeaa4bee75d9fa9a
Joey Huab <joey@evolution-x.org>
2022-05-04 04:20:34 +0900

    Revert "sepolicy: Deprecate LiveDisplay"
    This reverts commit f887e48b48d586bec9f9a25cfd8c3a0a703e330e.

---
+++ exynos/sepolicy.mk
@@ -0,0 +1,23 @@
+#
+# This policy configuration will be used by all exynos products
+# that inherit from PixelExperience
+#
+
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/evolution/sepolicy/exynos/private
+
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/evolution/sepolicy/exynos/dynamic \
+    device/evolution/sepolicy/exynos/system
+else
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/evolution/sepolicy/exynos/dynamic \
+    device/evolution/sepolicy/exynos/vendor
+endif
